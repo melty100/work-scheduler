d8dc15ab9320a8d7277ac700e528b02c687366cb
Connor <melty100@gmail.com>
2020-08-25 17:43:24 -0700

    Move index.html to root dir and modified script and css file paths

--- Develop/script.js
+++ Develop/script.js
@@ -7,13 +7,14 @@ class Record {
     }
 }
 
-//Used to compare temporal difference of our timeblocks 
+//Used to compare temporal differences of our timeblocks and the current time
 var everyHour = ["12am", "1am", "2am", "3am", "4am", "5am", "6am", "7am", "8am", "9am", "10am", "11am", 
-                    "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm"];
+                 "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm"];
 
 var timer = setInterval(checkTime, 1000);
 
 //Used for testing color changes for textareas at the turn of an hour. (9th index is 9am in everyHour array)
+//--------------------------------------- DELETE THIS ------------------------------------------------------
 var testCounter = 9;
 
 $(".saveBtn").click(function() {
@@ -25,16 +26,19 @@ $(".saveBtn").click(function() {
     let r = new Record(textAreaId, text);
     let schedule = localStorage.getItem("Schedule");
 
-    //If we dont have a schedule in local storage, then we create it here
+    //If we don't have a schedule in local storage, then we create it here
     if(schedule == null){
         schedule = [r];
     }
+    //If we do, then we either push a new record or overwrite an existing one
     else {
         schedule = JSON.parse(schedule);
+
         //If we have a record stored for a specific hour, find and write over it;
         let i = 0;
-        while(i < schedule.length && r.hour != schedule[i].hour){i++}
-
+        while(i < schedule.length && r.hour != schedule[i].hour){
+            i++;
+        }
         i < schedule.length ? schedule[i].text = text : schedule.push(r);
     }
 
@@ -46,6 +50,7 @@ function checkTime() {
     //Update time on jumbotron
     $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ssa'));
 
+    //loops through working hours in everyHours array -------------- DELETE THIS
     testCounter == 17 ? testCounter = 9 : testCounter++;
 
     let hour = moment().format('ha');
